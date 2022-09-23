1cea6af2a6679801425fe43268c6bfae217ca0a7
Timé Kadel <time.kadel@reseau.eseo.fr>
2022-09-23 12:29:54 +0200

    [-] - Removed unused mixins.

--- src/mixins/HTMLSprite.mixin.js
+++ src/mixins/HTMLSprite.mixin.js
@@ -5,9 +5,6 @@ import VUE3DRenderer from "@/plugins/visualizer/vue3d_renderer.js";
 export default {
   mounted() {
     VUE3DRenderer.addHTMLSprite(this);
-    this.$el.style.transformStyle = "preserve-3d"
     this.$el.style.display = "none"
-    this.$el.style.pointerEvents = "auto"
-    // this.$el.visibility = "visible"
   }
 }
