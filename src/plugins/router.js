830b299dde4533658e7e52a457df7742fc3f9e5b
Timé Kadel <time.kadel@reseau.eseo.fr>
2022-11-29 23:10:53 +0100

    [~] - Implemented multi level routing.

--- src/plugins/router.js
+++ src/plugins/router.js
@@ -3,11 +3,54 @@ import {
   createRouter
 } from "vue-router"
 import MainActivity from '@/views/activities/main/main.activity.vue';
+import SubActivity from '@/views/activities/sub/sub.activity.vue'
+import ScrollHandler from '@/plugins/utils/scroll.handler.js'
+import Sub from "@/minilanding/sub"
 
-export default createRouter({
-  routes: [
-    {path: "/", name: "App", component: MainActivity},
-    {path: "/:catchAll(.*)", name: "NotFound", redirect:{name: "App"}}
+const router = createRouter({
+  routes: [{
+      path: "/",
+      name: "App",
+      meta: {
+        meta: {
+          xPosition: 0,
+          yPosition: 0,
+        },
+      },
+      component: MainActivity
+    },
+    {
+      path: "/:subName",
+      name: "Sub",
+      beforeEnter:(to)=>{
+        if(!Object.keys(Sub).includes(to.path.replace('/',''))){
+          return {
+            name: "App"
+          }
+        }
+      },
+      meta: {
+        xPosition: 0,
+        yPosition: 0,
+        sub: true
+      },
+      component: SubActivity
+    },
+    {
+      path: "/:catchAll(.*)",
+      name: "NotFound",
+      redirect: {
+        name: "App"
+      }
+    }
   ],
   history: createWebHistory(),
 });
+
+router.beforeEach((to, from) => {
+  if (from.matched.length) {
+    from.matched[0].meta.yPosition = ScrollHandler.scrollY;
+  }
+})
+
+export default router;
