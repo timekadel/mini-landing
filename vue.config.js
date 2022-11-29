4777fc3d692128cdbfe558560ea2f3e1c5ff5b6d
Timé Kadel <time.kadel@reseau.eseo.fr>
2022-11-29 23:23:57 +0100

    [~] - Implemented basic SEO configuration.

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,4 @@
+/* webpack.config.js */
 module.exports = {
   chainWebpack: config => {
     config.resolve.alias.set('vue', '@vue/compat')
@@ -14,5 +15,35 @@ module.exports = {
           }
         }
       })
-    }
+      config.plugin("html").tap(args=>{
+        args[0] = {
+          // template: 'index.tmp.html',
+          template: '/public/index.html',
+          title: "Timé Kadel's portfolio",
+          keywords: "Timé, Kadel, timekadel, portfolio, roomz, roomz.tk, asls",
+          description: `Hi, my name is Timé Kadel, I'm an engineer in electronics and Information Technology who enjoys working on a various range of Innovative technologies and projects.`,
+          url: "https://timekadel.com",
+          image: "/images/tk.svg"
+        }
+        return args
+      })
+  },
+  /*configureWebpack: (config) => {
+    config.plugins = [
+      ...config.plugins,
+      new HtmlWebpackPlugin({
+        // template: 'index.tmp.html',
+        template: '/public/index.html',
+        title: "Timé Kadel's portfolio",
+        description: `
+          Hi, my name is Timé Kadel, 
+          I'm an engineer inElectronics 
+          and Information Technology 
+          who enjoys working on a various range 
+          of Innovative technologies and projects.`,
+        url: "https://timekadel.com",
+        image: "/images/tk.svg"
+      }),
+    ]
+  }*/
 }
