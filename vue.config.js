d404679a392609cc63ac5b8ffb1756893adf13fb
Timé Kadel <time.kadel@reseau.eseo.fr>
2022-11-30 10:07:28 +0100

    [~] - Set OG:Image link as an actual URL instead of relative link

--- vue.config.js
+++ vue.config.js
@@ -23,7 +23,7 @@ module.exports = {
           keywords: "Timé, Kadel, timekadel, portfolio, roomz, roomz.tk, asls",
           description: `Hi, my name is Timé Kadel, I'm an engineer in electronics and Information Technology who enjoys working on Innovative technologies and projects.`,
           url: "https://timekadel.com",
-          image: "/images/tk.png"
+          image: "https://timekadel.com/images/tk.png"
         }
         return args
       })
